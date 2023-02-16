cace92cb2c5edb6d04823987bedca0379034a16a
Andrii Chumak <me@achumak.name>
2023-02-16 16:54:31 +0100

    Moved new files to the root, removed company name from schema

--- new_file.py
+++ new_file.py
@@ -1,3 +1,3 @@
-from helpers.types import Dataset, Grain, Dimension, Fact, Join, MultiJoin
+from python.helpers.types import Dataset, Grain, Dimension, Fact, Join, MultiJoin
 
 # Build your model here using Dataset, Dimension, Fact, Join, MultiJoin and Grain functions. Use the other files in this project as examples.
