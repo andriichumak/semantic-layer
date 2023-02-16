cadd8171a6595ef2742a6f65e2c9f5a925dd583f
Andy Chumak <andrii.chumak@gooddata.com>
2023-02-16 14:32:00 +0100

    Added examples for YAML and Python

---
+++ python/helpers/types.py
@@ -0,0 +1,35 @@
+from dataclasses import dataclass, field
+from typing import Optional, NewType, Union, Any
+
+TagList = NewType('TagList', list[str])
+
+@dataclass
+class Dimension:
+    id: str
+    title: Optional[str]
+    tags: Optional[TagList]
+    source_column: Optional[str]
+
+@dataclass
+class Grain(Dimension):
+    pass
+
+@dataclass
+class Fact:
+    id: str
+
+@dataclass
+class Join:
+    dataset: Union['Dataset', str]
+    using: Union[list[str], str]
+    multivalue: Optional[bool]
+
+@dataclass
+class Dataset:
+    id: str
+    data_source: str
+    title: Optional[str]
+    include_all_fields: bool
+    tags: Optional[TagList]
+    fields: list[Union[Dimension, Fact]]
+    joins: list[Join]
---
+++ python/semantic_layer.py
@@ -0,0 +1,62 @@
+from helpers.types import Dataset, Attribute, Fact, Join
+
+Products = Dataset(
+    id = 'products',
+    data_source = 'ecommerce.products',
+
+    fields = [
+        Grain('product_id')
+    ],
+
+    include_all_fields = true
+)
+
+Customers = Dataset('customers', 'ecommerce.customers', fields = [ Grain('customer_id') ], include_all_fields = true)
+
+Dates = Dataset(
+    id = 'dates',
+    data_source = 'ecommerce.dates',
+
+    title = 'Order date',
+    description = 'Defines date / time dimension',
+    tags = [ 'ecommerce', 'date' ],
+    
+    fields = [
+        Grain('date'),
+        Dimension('year'),
+        Dimension('quarter'),
+        Dimension('week'),
+        Dimension('month'),
+        Dimension('day'),
+    ]
+)
+
+Promotions = Dataset('promotions', 'ecommerce.promotions', fields = [ Grain('promotion_id') ], include_all_fields = true)
+
+Orders = Dataset(
+    id = 'orders',
+    data_source = 'ecommerce.orders',
+
+    title = 'Orders',
+    description = 'Dataset with facts, containing a list of orders',
+    tags = [ 'ecommerce', 'orders' ],
+
+    fields = [
+        Grain('order_id'),
+        Fact(
+            id = 'revenue',
+            title = 'Revenue',
+            description = 'A total revenue for the order',
+            tags = [ 'ecommerce', 'revenue' ],
+            source_column = 'revenue'
+        ),
+        Fact('units_sold')
+    ],
+
+    joins = [
+        Join(dataset = Products, using = 'product_id', multivalue = false),
+        Join(Custoemrs, 'customer_id'),
+        Join(Dates, 'date'),
+        Join(Promotions, 'promotion_id')
+    ]
+)
