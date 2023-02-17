2d17177929302c77790dcfd6116d2f9a8796f3e0
Andy Chumak <andrii.chumak@gooddata.com>
2023-02-17 11:06:45 +0100

    Restructured folders

--- Excercise 2/Prototype B/new_file.py
+++ Excercise 2/Prototype B/new_file.py
@@ -1,3 +1,3 @@
-from python.helpers.types import Dataset, Grain, Dimension, Fact, Join, MultiJoin
+from helpers.types import Dataset, Grain, Dimension, Fact, Join, MultiJoin
 
 # Build your model here using Dataset, Dimension, Fact, Join, MultiJoin and Grain functions. Use the other files in this project as examples.
